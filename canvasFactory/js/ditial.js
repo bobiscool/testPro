09ffe6a4469bd8bcaba5e12717b8ea2bdbc3e583
bobiscool <tomisugly@163.com>
2017-08-23 18:21:27 +0800

    [Chg] canvas

--- canvasFactory/js/ditial.js
+++ canvasFactory/js/ditial.js
@@ -44,7 +44,7 @@ function drawGird(color, stepx, stepy) {
     context.strokeStyle = color;
     context.fillStyle = '#ffffff';
     context.lineWidth = 0.5;
-    context.fillRect(0, 0, context.canvas.width / 2, context.canvas.height / 2);
+    // context.fillRect(0, 0, context.canvas.width / 2, context.canvas.height / 2);
 
     for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
         context.beginPath();
@@ -72,7 +72,7 @@ function drawDial() {
     drawRing();
     drawTicksInnerCircle();
     drawTicks();
-    // drawAnnotations();
+    drawAnnotations();
 }
 
 function drawCentriod() {
@@ -121,64 +121,84 @@ function drawCentriodGuiwire() {
 }
 
 
-function drawRing(){
+function drawRing() {
     drawRingOuterCircle();
     context.save();
     context.strokeStyle = 'rgba(0,0,0,0.1)';
-    context.arc(circle.x,circle.y,circle.radius+R_I_R,0,Math.PI*2,false);
+    context.arc(circle.x, circle.y, circle.radius + R_I_R, 0, Math.PI * 2, false);
     context.fill();
     context.stroke();
     context.restore();
 }
 
-function drawRingOuterCircle(){
+function drawRingOuterCircle() {
     context.shadowColor = "rgba(0,0,0,0.7)";
     context.shadowOffsetX = 3;
     context.shadowOffsetY = 3;
     context.shadowBlur = 6;
     context.strokeStyle = T_D_S_S;
     context.beginPath();
-    context.arc(circle.x,circle.y,circle.radius+R_O_R,0,Math.PI*2,true);
+    context.arc(circle.x, circle.y, circle.radius + R_O_R, 0, Math.PI * 2, true);
     context.stroke();
-    context.restore(); 
+    context.restore();
 }
 
-function drawTicksInnerCircle(){
+function drawTicksInnerCircle() {
     context.save();
     context.beginPath();
     context.strokeStyle = "rgba(0,0,0,0.1)";
-    context.arc(circle.x,circle.y,circle.radius+R_I_R-T_W,0,Math.PI*2,false);
+    context.arc(circle.x, circle.y, circle.radius + R_I_R - T_W, 0, Math.PI * 2, false);
     context.stroke();
     context.restore();
 }
 
 
-function drawTick(angle,radius,cnt){
-    var tickWidth = cnt%4 === 0?T_W:T_W/2;
+function drawTick(angle, radius, cnt) {
+    var tickWidth = cnt % 4 === 0 ? T_W : T_W / 2;
     context.beginPath();
-    context.moveTo(circle.x+Math.cos(angle)*(radius - tickWidth),circle.y+Math.sin(angle)*(radius - tickWidth));
-    context.lineTo(circle.x+Math.cos(angle)*radius,circle.y+Math.sin(angle)*radius);
+    context.moveTo(circle.x + Math.cos(angle) * (radius - tickWidth), circle.y + Math.sin(angle) * (radius - tickWidth));
+    context.lineTo(circle.x + Math.cos(angle) * radius, circle.y + Math.sin(angle) * radius);
     context.strokeStyle = T_S_S_S;
     context.stroke();
 
 }
 
-function drawTicks(){
-    var radius = circle.radius+R_I_R,
-    A_M = 2*Math.PI,
-    A_D = Math.PI/64,
-    tickWidth;
+function drawTicks() {
+    var radius = circle.radius + R_I_R,
+        A_M = 2 * Math.PI,
+        A_D = Math.PI / 64,
+        tickWidth;
 
-    for(var angle=0,cnt=0;angle < A_M;angle +=A_D,cnt++){
-        drawTick(angle,radius,cnt++);
+    for (var angle = 0, cnt = 0; angle < A_M; angle += A_D, cnt++) {
+        drawTick(angle, radius, cnt++);
     }
     context.restore();
 }
 
 
+function drawAnnotations() {
+    var radius = circle.radius + R_I_R;
+    context.save();
+    context.fillStyle = A_F_S;
+    context.font = A_T_S + 'px Helvetica';
 
+    for (var angle = 0; angle < 2 * Math.PI; angle += Math.PI / 8) {
+        context.beginPath();
+        context.fillText((angle*180/Math.PI).toFixed(0),circle.x + Math.cos(angle) * (radius - T_W*2), circle.y - Math.sin(angle) * (radius -  T_W*2))
+    }
 
+    context.restore();
+};
 
+context.shadowColor = 'ragba(0,0,0,0.4)';
+context.shadowOffsetX = 2;
+context.shadowOffsetY = 2;
+context.shadowBlur = 4;
+
+context.textAlign = 'center';
+context.textBaseline = 'middle';
+
+drawGird('lightgray',10,10);
 
 
 
