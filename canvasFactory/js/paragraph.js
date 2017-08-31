2653155f1ba464667f4401bb4bdf660dd425e621
bobiscool <tomisugly@163.com>
2017-08-31 18:07:11 +0800

    [Chg]  添加段落对象

--- canvasFactory/js/paragraph.js
+++ canvasFactory/js/paragraph.js
@@ -2,7 +2,7 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-08-31 09:25:53 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-08-31 09:52:16
+ * @Last Modified time: 2017-08-31 18:06:29
  * 段落对象
  */
 
@@ -11,7 +11,7 @@ paragraph = function (context, left, top, imagedata, cursor) {
     this.left = left;
     this.top = top;
     this.drawingSurface = imagedata;
-     this.cursor = cursor;
+    this.cursor = cursor;
     this.lines = [];
     this.activeLine = undefined;
     this.blinkingInterval = undefined;
@@ -20,15 +20,45 @@ paragraph = function (context, left, top, imagedata, cursor) {
 
 
 paragraph.prototype = {
-    addLine:function(line){
-          this.lines.push(line);
-          this.activeLine = line;
-          this.moveCursor(line.left,line.bottom)
+    addLine: function (line) {
+        this.lines.push(line);
+        this.activeLine = line;
+        this.moveCursor(line.left, line.bottom)
     },
-    moveCursor:function(){
-        this.cursor.erase(this.context,this.drawingSurface);
-        htis.cursor.draw(this.context,x,y);
-         
-    }    
+    moveCursor: function () {
+        this.cursor.erase(this.context, this.drawingSurface);
+        this.cursor.draw(this.context, x, y);
+        this.blinkCursor(x, y);
+    },
+    moveCursorCloseTo: function () {
+        var line = this.getLine(y);// 获取在第几行
+        if (line) {
+            line.caret = this, getColumn(line, x);
+            this.active
+        }
+    },
+    insert: function (text) {
+        var t = this.activeLine.text.substring(0, this.activeLine.caret),
+            w = this.activeLine.measureText(t).width;
+
+        this.activeLine.erase(this.context, this.drawingSurface);
+        this.activeLine.insert(text);
+        this.moveCursor(this.activeLine.left + w, this.activeLine.bottom);
+        this.activeLine.draw(this.context);
+    },
+   newline:function(){
+       var textBeforeCursor = this.activeLine.text.substring(0,this.activeLine.caret),
+       textAfterCursor = this.activeLine.text.substring(this.activeLine.caret),
+       height = this.context.measureText('W').width+this.context.measureText('W').width/6;
+       bottom = this.activeLine.bottom + height,
+       activeIndex,
+       line;
+
+       this.erase(this.context,this.drawingSurface);
+       //擦除掉文本行
+       this.activeLine.text = textBeforeCursor;
+       // 记录下以前的文字 
+       // 
+   } 
 
 }
