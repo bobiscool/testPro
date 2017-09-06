647239d761153939bf03ba8cf6022ea395e872a7
bobiscool <tomisugly@163.com>
2017-09-06 13:40:07 +0800

    [Chg]  折腾好久

--- canvasFactory/js/test4.js
+++ canvasFactory/js/test4.js
@@ -2,24 +2,28 @@ var canvas = document.getElementById('haha'),
     context = canvas.getContext('2d');
 var sky = new Image();
     var skyOffset = 0;
+    var a =0 ;
     var fps=10;
     var lastTime =new Date;
+    var ani=null;
 
     function calcFps (now){
         var fps = 1000/(now - lastTime);
-        console.log(fps);
+        // console.log(fps);
         lastTime = now;
         return fps;
     }
 
     function draw(){
+        // console.log(a);
+      
         // context.clearRect(0,0,canvas.width,canvas.height);
         context.save();
-       skyOffset = skyOffset<canvas.width?skyOffset+0.1/fps:0;
+       console.log(fps);        
+       skyOffset += (skyOffset<canvas.width)?30/fps:0;
        context.save();
-       console.log(skyOffset);
-
-       context.translate(-skyOffset,0);
+      
+       context.translate(-1*skyOffset,0);
 
        context.drawImage(sky,0,0);
        context.drawImage(sky,sky.width-2,0);
@@ -32,10 +36,10 @@ var sky = new Image();
          if(!now){
              now = +new Date();
          }
-            fps = calcFps(now);
-            
+         let tem = calcFps(now)
+            if(tem>0)fps=tem;
             draw();
-            requestNextAnimationFrame(animate);
+          ani = window.requestAnimationFrame(animate);
     }
 
 
