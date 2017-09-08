346b5cc4d495ae3e26445a12dec3b2b7112a8794
bobiscool <tomisugly@163.com>
2017-09-08 10:29:47 +0800

    [Chg]  理解两种不同的模式

--- canvasFactory/cons/SpriteSheetPainter.js
+++ canvasFactory/cons/SpriteSheetPainter.js
@@ -2,8 +2,17 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-08 10:16:33 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-08 10:21:09
+ * @Last Modified time: 2017-09-08 10:29:01
  */
+
+ /**
+  * 而把不同的命令 -不同的动画 那是命令模式的应用 
+  * 把一个动画里面要素分解为不同的对象  是策略模式的一种应用  
+  * 
+  * 
+  * 
+  *
+  */
 
 var SpriteSheetPainter = function (cells) {
     this.cells = cells || [];
