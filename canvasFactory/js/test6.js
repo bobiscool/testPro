e5dd58a7e12779e9168be05711499911bb0ec43f
bobiscool <tomisugly@163.com>
2017-09-08 17:55:12 +0800

    继续精灵图动画

--- canvasFactory/js/test6.js
+++ canvasFactory/js/test6.js
@@ -6,7 +6,7 @@ var canvas = document.getElementById('canvas'),
     sheetImg.src = "./js/sheet.png"
 
 
-    runnerCells = [
+    var runnerCells = [
           { left: 0,   top: 0, width: 47, height: 64 },
       { left: 55,  top: 0, width: 44, height: 64 },
       { left: 107, top: 0, width: 39, height: 64 },
@@ -16,9 +16,21 @@ var canvas = document.getElementById('canvas'),
       { left: 320, top: 0, width: 42, height: 64 },
       { left: 380, top: 0, width: 35, height: 64 },
       { left: 425, top: 0, width: 35, height: 64 },
-    ];
+    ],
 
     runInplace = {
         lastAdvance:0,
         PAGECLIP_INTERVAL:100,
+        execute:function(sprite,context,time){
+                   if(time - this.lastAdvance > this.PAGECLIP_INTERVAL){
+                       sprite.painter.advance();
+                       this.lastAdvance = time;
+                   }
+        }
+    },
+    moveLeftToRight={
+        lastMove:0,
+        execute:function(){
+            
+        }
     }
