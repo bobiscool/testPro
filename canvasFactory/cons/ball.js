5995358fed4a913734b14693bdfcb7022480acb2
bobiscool <tomisugly@163.com>
2017-09-11 10:26:02 +0800

    [Chg] 添加物理效果

---
+++ canvasFactory/cons/ball.js
@@ -0,0 +1,35 @@
+var G = 9.8;
+var fps = 10;
+var lastTime = new Date;
+
+function calcFps(now) {
+    var fps = 1000 / (now - lastTime);
+    // console.log(fps);
+    lastTime = now;
+    if(fps<0){
+        fps = 100;
+    }
+    return fps;
+}
+
+function Ball(x, y, vX, vY) {
+    this.x = x;
+    this.y = y;
+    this.vX = vX;
+    this.vY = vY;
+}
+
+Ball.prototype = {
+    updatePosition: function () {
+        this.x += this.vX;
+        this.y += this.vY;
+    },
+    updateVy: function () {
+        let now = new Date();
+           this.vY  += G/calcFps(now);
+    },
+    paint:function(){
+        
+    }    
+
+}
