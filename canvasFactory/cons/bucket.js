fb3a5be2ba90653ef72ab0ff365805b7407f8bce
bobiscool <tomisugly@163.com>
2017-09-13 10:26:09 +0800

    [Chg] bucket

--- canvasFactory/cons/bucket.js
+++ canvasFactory/cons/bucket.js
@@ -1,10 +1,23 @@
+function Ellipse(context, x, y, a, b) {
+    var step = (a > b) ? 1 / a : 1 / b;
+    context.beginPath();
+    context.moveTo(x + a, y);
+    for (var i = 0; i < 2 * Math.PI; i += step) {
+        context.lineTo(x + a * Math.cos(i), y + b * Math.sin(i));
+    }
+
+    context.closePath();
+    context.fill();
+}
+
 function bucket(x, y) {
     this.x = x;
     this.y = y;
 }
 
 bucket.prototype = {
-    drawCircle:function(context){
-       context.arc(this.x,this.y)
+    paint:function(context){
+      Ellipse(context,this.x,this.y,3,5);
+      cntext.rect(x,y,10,20);
     }
 }
