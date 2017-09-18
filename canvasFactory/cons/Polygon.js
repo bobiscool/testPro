80e36c055c138673befe9d6844379643db7e4484
bobiscool <tomisugly@163.com>
2017-09-18 11:33:06 +0800

    [Chg] add polygon

---
+++ canvasFactory/cons/Polygon.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Thunderball.Wu 
+ * @Date: 2017-09-18 11:30:20 
+ * @Last Modified by: Thunderball.Wu
+ * @Last Modified time: 2017-09-18 11:32:36
+ * 多边形对象
+ */
+
+ var Point = function(x,y){
+     this.x = x;
+     this.y = y;
+ };
+ 
+ var Polygon = function(){
+     this.points = [];
+     this.strokeStyle = 'blue';
+     this.fillStyle = 'white'
+ }
