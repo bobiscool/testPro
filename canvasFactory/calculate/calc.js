405428b84057448c6c8d728acbef7fb4927d1dcf
bobiscool <tomisugly@163.com>
2017-09-19 15:09:17 +0800

    [Chg] add exeRank for 计算优先级

--- canvasFactory/calculate/calc.js
+++ canvasFactory/calculate/calc.js
@@ -2,10 +2,12 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-19 12:47:19 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-19 15:01:40
+ * @Last Modified time: 2017-09-19 15:08:48
  * calc.js
  * 用于计算的js库
  */
+
+ var expExample = ['1','+','2','+',"sin(",'3',')']
 
  var expRank = {
      "(":0,
@@ -25,6 +27,8 @@ function Calc(exp){
 function _Calc(exp){
  // Calc 随身携带的包
    this.exp = exp;
+   // 由于有 sin cos 这些特殊的函数 我建议 传入的exp 最好不要是字符串 就是一个数组
+   this.expList = exp.split('');
    this.rightStack = [];
    //左右两边的栈
    this.leftStack = [];
@@ -36,5 +40,8 @@ _Calc.prototype = {
         //初始化解析
         
     },
+    _calculate:function(){
+        this
+    }
 
 }
