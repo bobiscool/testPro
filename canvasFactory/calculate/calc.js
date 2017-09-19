59c50f36ffa341a37da6e8b35a47efd9f5f80ddf
bobiscool <tomisugly@163.com>
2017-09-19 13:00:56 +0800

    [Chg] add calc js

--- canvasFactory/calculate/calc.js
+++ canvasFactory/calculate/calc.js
@@ -2,18 +2,26 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-19 12:47:19 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-19 12:49:52
+ * @Last Modified time: 2017-09-19 13:00:21
  * calc.js
  * 用于计算的js库
  */
 
 function Calc(exp){
-   this.exp = exp;
-
+  
 }
 
-Calc.prototype = {
-    _init:function(){
-        //初始化解析·
+function _Calc(exp){
+ // Calc 随身携带的包
+   this.exp = exp;
+   this.right = [];
+   this.left = [];
+}
+
+
+_Calc.prototype = {
+    _init:function(exp){
+        //初始化解析
+       
     }
 }
