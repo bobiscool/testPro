1bbcb428ce69d8af9c83c1f5ae2e026a28f9c888
bobiscool <tomisugly@163.com>
2017-09-19 16:26:44 +0800

    [Chg] add genSubfix

--- canvasFactory/calculate/calc.js
+++ canvasFactory/calculate/calc.js
@@ -2,7 +2,7 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-19 12:47:19 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-19 16:22:34
+ * @Last Modified time: 2017-09-19 16:26:29
  * calc.js
  * 用于计算的js库
  */
@@ -64,13 +64,10 @@ _Calc.prototype = {
                         this.symbolStack.push(item);
                     }else{
                                  // 如果当前遇到的符号 优先级 小于 等于 前面的 那就直接 弹出栈
-
-                                 
+                        this.numberStack.push(this.symbolStack.pop())
                     }
                 }
-                this.symbolStack[this.symbolStack.length-1]
 
-               this.symbolStack.push(item);
             }
         }, this);
     }
