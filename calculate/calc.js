2e9350db99e8cff58ff086c6bf78d0b1947c7b3d
bobiscool <tomisugly@163.com>
2017-09-20 14:55:12 +0800

    [Chg] Fix sin

--- calculate/calc.js
+++ calculate/calc.js
@@ -2,12 +2,12 @@
  * @Author: Thunderball.Wu 
  * @Date: 2017-09-19 12:47:19 
  * @Last Modified by: Thunderball.Wu
- * @Last Modified time: 2017-09-20 14:01:48
+ * @Last Modified time: 2017-09-20 14:53:37
  * calc.js
  * 用于计算的js库
  */
 
-var expExample = ['3', '+', '2', '-', '5', '/', '5', '+', 'sin(' + '50' + ')']
+var expExample = ['3', '+', '2', '-', '5', '/', '5', '+', 'sin(' , '50' , ')']
 
 var b = "3+2-5/5"
 var c = "1-(9-7)/2"
@@ -68,7 +68,7 @@ _Calc.prototype = {
                 //是符号的时候
                 let _temArray = [];
                 let _l = this.calcStack.length;
-                _temArray = this.calcStack.splice(_l-_getMu[item]);
+                _temArray = this.calcStack.splice(_l - _getMu[item]);
                 // _temArray.unshift(this.calcStack.pop());//拿到数字堆栈 最后两数
                 // _temArray.unshift(this.calcStack.pop());//拿到数字堆栈 最后两数
                 console.log('----', this.calcStack);
@@ -104,6 +104,18 @@ _Calc.prototype = {
                     // console.log('oldSym', oldSymbol);
                     console.log('bracketNum', this.bracketNum.length);
                     //外加一个限制 如果是 ( 那那就需要等)
+
+                    // this._monocary(item);//
+
+                    if (item == "sin(") {
+                        // this.numberStack.push('sin');//先放入
+                        console.log('sin',this.numberStack)
+                        this.symbolStack.push('sin');
+                        this.bracketNum.push(this.symbolStack.length - 1);//记录下位置
+                        return false;
+                    }
+
+
                     if (item == ")" && this.bracketNum.length > 0) {
                         // console.log(this.bracketNum);
                         let _tem = this.bracketNum.pop();
@@ -112,7 +124,9 @@ _Calc.prototype = {
 
                         let _tem2 = this.symbolStack.splice(_tem);
                         console.log('_tem2', _tem2);
+                        if(_tem2[0]=='('){
                         _tem2.shift();
+                        }
                         //    this.symbolStack.pop();//拿掉 (
                         console.log('这个时候', this.symbolStack);
                         this.numberStack = this.numberStack.concat(_tem2);
@@ -178,8 +192,8 @@ _Calc.prototype = {
          */
 
         if (item == "sin(") {
-            this.numberStack.push('sin');//先放入
-            this.symbolStack.push('(');
+            // this.numberStack.push('sin');//先放入
+            this.symbolStack.push('sin');
             this.bracketNum.push(this.symbolStack.length - 1);//记录下位置
         }
 
@@ -187,18 +201,18 @@ _Calc.prototype = {
 }
 
 
-var  _getMu = {// 参与运算的运算因子 数目
-    "+":2,
-    "-":2,
-    "*":2,
-    "/":2,
-    "sin":1,
-    "cos":1,
+var _getMu = {// 参与运算的运算因子 数目
+    "+": 2,
+    "-": 2,
+    "*": 2,
+    "/": 2,
+    "sin": 1,
+    "cos": 1,
 }
 
 
 
-var _Math = {
+var _Math = { // 具体的算术运算符
     "+": function (a) {
         return Number(a[0]) + Number(a[1]);
     },
@@ -211,8 +225,9 @@ var _Math = {
     "/": function (a) {
         return Number(a[0]) / Number(a[1]);
     },
-    "sin": function(a){
-       return Math.sin(Number(a[0]));
+    "sin": function (a) {
+        console.log("计算sin",a)
+        return Math.sin(Number(a[0]));
     }
 }
 
@@ -223,23 +238,6 @@ var _Phase = function () {
 }
 
 
-function _monocary(item, symbolStack, numberStack) {
-    /**
-     * 所有的单目运算 都在等待一个 ) 也就是 一旦有单目符号 便要压入栈中 
-       然后 brackNum 计数
-       在遇到 括) 之后 弹出 并且将 所有与之相关的符号都弹出 而这个 也放在 所有符号的后面
-       然后单目运算它
-   
-       单目运算符 优先级非常高
-     */
-
-    if (item == "sin(") {
-        numberStack.push(item);
-        this.bracketNum.push(this.symbolStack.length - 1);
-
-    }
-
-}
 
 
 
@@ -248,4 +246,5 @@ function _monocary(item, symbolStack, numberStack) {
 
 
 
-Calc(k);
+
+Calc(expExample);
